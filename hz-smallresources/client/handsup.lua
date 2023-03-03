0061ae945cf67556b34cda3e328bbf3c51b8d9ae
BruceFX <89119359+BruceFX@users.noreply.github.com>
2023-03-02 23:17:47 -0500

    Update handsup.lua
    Don't need qb-policejob.

--- hz-smallresources/client/handsup.lua
+++ hz-smallresources/client/handsup.lua
@@ -1,27 +1,21 @@
-local animDict = "missminuteman_1ig_2"
-local anim = "handsup_base"
-local handsup = false
-local disableHandsupControls = {24, 25, 47, 58, 59, 63, 64, 71, 72, 75, 140, 141, 142, 143, 257, 263, 264}
-
-RegisterCommand('hu', function()
-    local ped = PlayerPedId()
-    if not HasAnimDictLoaded(animDict) then
-        RequestAnimDict(animDict)
-        while not HasAnimDictLoaded(animDict) do
-            Wait(10)
+Citizen.CreateThread(function()
+    local dict = "missminuteman_1ig_2"
+    
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(100)
+	end
+    local handsup = false
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustPressed(1, 323) then --Start holding X
+            if not handsup then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+                handsup = true
+            else
+                handsup = false
+                ClearPedTasks(GetPlayerPed(-1))
+            end
         end
     end
-    handsup = not handsup
-    if exports['qb-policejob']:IsHandcuffed() then return end
-    if handsup then
-        TaskPlayAnim(ped, animDict, anim, 8.0, 8.0, -1, 50, 0, false, false, false)
-        exports['qb-smallresources']:addDisableControls(disableHandsupControls)
-    else
-        ClearPedTasks(ped)
-        exports['qb-smallresources']:removeDisableControls(disableHandsupControls)
-    end
-end, false)
-
-RegisterKeyMapping('hu', 'Put your hands up', 'KEYBOARD', 'X')
-
-exports('getHandsup', function() return handsup end)
+end)
