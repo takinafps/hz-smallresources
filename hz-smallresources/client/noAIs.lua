c5397fb51c5c9c66da0cb7d51d42b219584dff2f
BruceFX <89119359+BruceFX@users.noreply.github.com>
2023-03-04 15:13:36 -0500

    Created noAIs.lua
    Remove AIs from your server.

---
+++ hz-smallresources/client/noAIs.lua
@@ -0,0 +1,17 @@
+-- Remove ReduceNPC.lua and calmallai.lua to make it work.
+
+-- DensityMultiplier = 0.00
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 	    Citizen.Wait(0)
+-- 	    SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
+-- 	    SetPedDensityMultiplierThisFrame(DensityMultiplier)
+-- 	    SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
+-- 	    SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
+-- 	    SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+		
+-- 		local playerPed = GetPlayerPed(-1)
+-- 		local pos = GetEntityCoords(playerPed) 
+-- 		RemoveVehiclesFromGeneratorsInArea(pos['x'] - 500.0, pos['y'] - 500.0, pos['z'] - 500.0, pos['x'] + 500.0, pos['y'] + 500.0, pos['z'] + 500.0);
+-- 	end
+-- end)
