363091f28197c22ede689aac40790da36b166261
Takina <89119359+takinafps@users.noreply.github.com>
2024-08-27 18:59:01 -0400

    Update sv_version_check.lua

--- hz-smallresources/sv_version_check.lua
+++ hz-smallresources/sv_version_check.lua
@@ -1,22 +1,20 @@
 local label =
 [[ 
-  //
-  || 	$$\   $$\ $$$$$$$$\ 
-  || 	$$ |  $$ |\____$$  |
-  || 	$$ |  $$ |    $$  / 
-  || 	$$$$$$$$ |   $$  /  
-  || 	$$  __$$ |  $$  /   
-  || 	$$ |  $$ | $$  /    
-  || 	$$ |  $$ |$$$$$$$$\ 
-  || 	\__|  \__|\________|					  
-  || 
-  ||]]
+ //
+ || ███╗   ██╗██╗  ██╗
+ || ████╗  ██║██║  ██║
+ || ██╔██╗ ██║███████║
+ || ██║╚██╗██║██╔══██║
+ || ██║ ╚████║██║  ██║
+ || ╚═╝  ╚═══╝╚═╝  ╚═╝
+ || 
+ ||]]
 
 function GetCurrentVersion()
 	return GetResourceMetadata( GetCurrentResourceName(), "version" )
 end
 
-PerformHttpRequest( "https://BruceFX.github.io/hz-smallresources_web/version.txt", function( err, text, headers )
+PerformHttpRequest( "https://BruceFX.github.io/NH-smallresources_web/version.txt", function( err, text, headers )
 	Citizen.Wait( 2000 )
 
 	print( label )
@@ -29,15 +27,15 @@ PerformHttpRequest( "https://BruceFX.github.io/hz-smallresources_web/version.txt
 		print( "  ||    Latest recommended version: " .. text .."\n  ||" )
 
 		if ( text ~= curVer ) then
-			print( "  ||    ^1Your HZ small resources version is outdated, there might be more script that you might like, go to https://github.com/BruceFX/hz-smallresources to update." )
+			print( "  ||    ^1Your NH small resources version is outdated, there might be more script that you might like, go to https://github.com/BruceFX/NH-smallresources to update." )
 		else
-			print( "  ||    ^2HZ Small Resouces is up to date!\n^0  ||\n  \\\\\n" )
+			print( "  ||    ^2NH Small Resouces is up to date!\n^0  ||\n  \\\\\n" )
 		end
 	else
 		print( "  ||    ^1There was an error getting the latest version information.\n^0  ||\n  \\\\\n" )
 	end
 
-	if ( GetCurrentResourceName() ~= "hz-smallresources" ) then
-		print( "^1ERROR: Resource name is not hz-smallresources, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as hz-smallresources^0\n\n" )
+	if ( GetCurrentResourceName() ~= "NH-smallresources" ) then
+		print( "^1ERROR: Resource name is not NH-smallresources, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as NH-smallresources^0\n\n" )
 	end
 end )
